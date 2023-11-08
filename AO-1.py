808f6de2b188ff34fe25bad3cebc29b874a25896
Padma Abinaya <103116460+abi1395@users.noreply.github.com>
2023-11-08 15:21:57 +0400

    Add files via upload

---
+++ AO-1.py
@@ -0,0 +1,16 @@
+Python 3.12.0 (v3.12.0:0fb18b02c8, Oct  2 2023, 09:45:56) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print(10+5)
+15
+>>> print(10-5)
+5
+>>> print(10*5)
+50
+>>> print(10/5)
+2.0
+>>> print(10%5)
+0
+>>> print(10**5)
+100000
+>>> print(10//2)
+5
