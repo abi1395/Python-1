9f1821572c9a6e6e3ea48ef76ce62126f09b1fdc
Padma Abinaya <103116460+abi1395@users.noreply.github.com>
2023-12-14 16:25:51 +0530

    Add files via upload

---
+++ variable assignment.py
@@ -0,0 +1,30 @@
+Python 3.12.0 (v3.12.0:0fb18b02c8, Oct  2 2023, 09:45:56) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> my_dogs = 2
+>>> my_dogs
+2
+>>> my_dogs = ['sammy','frankie']
+>>> my_dogs
+['sammy', 'frankie']
+>>> a = 5
+>>> a
+5
+>>> a = 10
+>>> a
+10
+>>> a + a
+20
+>>> a = a+10
+>>> a
+20
+>>> a += 10
+>>> a
+30
+>>> a *= 2
+>>> a
+60
+>>> type(a)
+<class 'int'>
+>>> a = (1,2)
+>>> type(a)
+<class 'tuple'>
