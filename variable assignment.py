c09519da305bf0cf2e576661de6df2416eb5b827
Padma Abinaya <103116460+abi1395@users.noreply.github.com>
2023-12-14 16:26:30 +0530

    variable assignment.py

--- variable assignment.py
+++ variable assignment.py
@@ -1,5 +1,4 @@
-Python 3.12.0 (v3.12.0:0fb18b02c8, Oct  2 2023, 09:45:56) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
-Type "help", "copyright", "credits" or "license()" for more information.
+
 >>> my_dogs = 2
 >>> my_dogs
 2
