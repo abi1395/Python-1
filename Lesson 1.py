4313e523f52873e1e212a4276fcaa208f80a4245
Padma Abinaya <103116460+abi1395@users.noreply.github.com>
2023-12-14 16:16:23 +0530

    Lesson 1.py

--- Lesson 1.py
+++ Lesson 1.py
@@ -1,44 +1,43 @@
-Python 3.12.0 (v3.12.0:0fb18b02c8, Oct  2 2023, 09:45:56) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
-Type "help", "copyright", "credits" or "license()" for more information.
+
 >>> #1,2,3,4,5...intergers
 >>> #1.2,0.5,2e...floating numbers
->>> 
->>> #addition
+
+#addition
 >>> 2+1
 3
->>> 
->>> #subtraction
+
+#subtraction
 >>> 2-1
 1
->>> 
->>> #multiplication
+
+#multiplication
 >>> 2*2
 4
->>> 
->>> #division
+
+#division
 >>> 3/2
 1.5
->>> 
->>> #floor division
+
+#floor division
 >>> 7//4
 1
->>> 
->>> #modulo
+
+#modulo
 >>> 7%4
 3
->>> 
->>> #powers
+
+#powers
 >>> 2**3
 8
->>> 
+
 >>> 4**0.5
 2.0
 >>> 2+10*10+3
 105
 >>> (2+10) * (10+3)
 156
->>> 
+
 >>> a=5
 >>> a+a
 10
->>> a=10
+
