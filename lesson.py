960e109fddf5793ef62d8a9d128ab8acfb63971f
Padma Abinaya <103116460+abi1395@users.noreply.github.com>
2023-12-14 16:17:14 +0530

    Add files via upload

---
+++ lesson.py
@@ -0,0 +1,7 @@
+Python 3.12.0 (v3.12.0:0fb18b02c8, Oct  2 2023, 09:45:56) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> my_income = 100
+>>> tax_rate = 0.1
+>>> my_taxes = my_income * tax_rate
+>>> my_taxes
+10.0
