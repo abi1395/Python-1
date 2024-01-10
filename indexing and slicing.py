5f2de6cad77b48d354b74c77c742099add517182
Padma Abinaya <103116460+abi1395@users.noreply.github.com>
2024-01-10 22:37:16 +0530

    Add files via upload

---
+++ indexing and slicing.py
@@ -0,0 +1,10 @@
+my_list = ['one','two','three',4,5]
+print(my_list[0])
+print(my_list[1:])
+print(my_list[:3])
+my_list = my_list + ['new item']
+print(my_list)
+my_list = my_list + ['add new item']
+print(my_list)
+my_list = my_list * 2
+print(my_list)
