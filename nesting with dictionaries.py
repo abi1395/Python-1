bc0bbf52618329c348b1e6795d85c5413211c1c5
Padma Abinaya <103116460+abi1395@users.noreply.github.com>
2024-01-12 11:37:11 +0400

    Add files via upload

---
+++ nesting with dictionaries.py
@@ -0,0 +1,6 @@
+d = {'key1' : {'nestkey' : {'subnestkey' : 'value'}}}
+print(d['key1']['nestkey']['subnestkey'])
+d = {'key1': 1, 'key2': 2, 'key3': 3}
+print(d.keys())
+print(d.values())
+print(d.items())
