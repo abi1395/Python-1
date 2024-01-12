e0ae76e3af3d66ea7a67702db62f585ad357c80d
Padma Abinaya <103116460+abi1395@users.noreply.github.com>
2024-01-12 11:02:41 +0400

    Add files via upload

---
+++ basic list methods.py
@@ -0,0 +1,13 @@
+list1 = [1,2,3]
+list1.append ('append me!')
+print(list1)
+list1.pop(0)
+print(list1)
+popped_item = list1.pop()
+print(popped_item)
+new_list = ['a','b','c','d','e']
+print(new_list)
+new_list.reverse()
+print(new_list)
+new_list.sort()
+print(new_list)
