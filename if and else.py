9f6ba6b4c39a7bf466030368ce4e532f6431e043
Padma Abinaya <103116460+abi1395@users.noreply.github.com>
2024-02-08 12:03:47 +0400

    Add files via upload

---
+++ if and else.py
@@ -0,0 +1,28 @@
+x = False
+if x:
+    print('x was true')
+else:
+    print('i will print if x not true')
+if True:
+    print('it is true')
+loc = 'bank'
+if loc == 'auto shop':
+    print('welcome to auto shop')
+elif loc == 'bank':
+    print('welcome to bank')
+else:
+    print('where are you?')
+
+person = 'sam'
+if person == 'sam':
+    print('welcome sam')
+else:
+    print('welcome, what is your name?')
+
+person = 'George'
+if person == 'sam':
+    print('welcome sam')
+elif person == 'George':
+    print('welcome George')
+else: 
+    print('welcome what is your name')
