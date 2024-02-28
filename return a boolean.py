9d0ab3bbe171eeb7dc3b6ad33b0f8d71e63068cf
Padma Abinaya <103116460+abi1395@users.noreply.github.com>
2024-02-28 11:13:10 +0400

    return a boolean.py

--- return a boolean.py
+++ return a boolean.py
@@ -28,3 +28,16 @@ for stock,price in stock_prices:
 for stock,price in stock_prices:
     print(price)
 
+work_hours = [('Abby',100),('Billy',400),('Cassie',800)]
+def employee_check(work_hours):
+    current_max = 0
+    employee_of_month = ''
+
+    for employee,hours in work_hours:
+        if hours > current_max:
+            current_max = hours
+            employee_of_month = employee
+        else:
+            pass
+    return (employee_of_month,current_max)
+print(employee_check(work_hours))
